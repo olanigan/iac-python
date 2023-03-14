5891c9a330a2174309b14ea9130225dfaf1d94b2
Ibrahim Olanigan <salalite@gmail.com>
2023-03-13 23:43:50 -0400

    Add Provider access and instance resource

--- devops/tf/main.tf
+++ devops/tf/main.tf
@@ -6,4 +6,20 @@ terraform {
         version = "1.25.0"
     }
  }
-}
+
+ }
+
+provider "linode" {
+    token = var.linode_pat_token
+    }
+resource "linode_instance" "cfe-pyapp" {
+    count = "1"
+    image = "linode/ubuntu20.04"
+    label = "pyapp-${count.index + 1}"
+    group = "Iac-Learner"
+    region = "us-east"
+    type = "g6-nanode-1"
+    authorized_keys = [ var.authorized_key ]
+    root_pass = var.root_user_pw
+    tags = [ "python", "cfe" ]
+    }
---
+++ devops/tf/variables.tf
@@ -0,0 +1,14 @@
+variable "linode_pat_token" {
+  sensitive = true
+}
+
+variable "authorized_key" {
+  sensitive = true
+}
+
+variable "root_user_pw" {
+  sensitive = true
+}
+
+
+
