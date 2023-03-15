d6e017ff987232fbf06e27874e85f35ae91a64bd
Ibrahim Olanigan <salalite@gmail.com>
2023-03-15 00:05:29 -0400

    Add backend for tfstate and docker install

--- devops/tf/main.tf
+++ devops/tf/main.tf
@@ -1,19 +1,25 @@
 terraform {
- required_version = ">= 0.15"
- required_providers {
-    linode = {
-        source = "linode/linode"
-        version = "1.25.0"
+    required_version = ">= 0.15"
+    required_providers {
+        linode = {
+            source = "linode/linode"
+            version = "1.25.0"
+        }
     }
+
+    backend "s3" {
+        skip_credentials_validation = true
+        skip_region_validation = true
+        }
  }
 
- }
+
 
 provider "linode" {
     token = var.linode_pat_token
     }
 resource "linode_instance" "cfe-pyapp" {
-    count = "2"
+    count = "1"
     image = "linode/ubuntu20.04"
     label = "pyapp-${count.index + 1}"
     group = "Iac-Learner"
@@ -22,4 +28,19 @@ resource "linode_instance" "cfe-pyapp" {
     authorized_keys = [ var.authorized_key ]
     root_pass = var.root_user_pw
     tags = [ "python", "cfe" ]
+
+    provisioner "remote-exec" {
+        connection {
+            host = "${self.ip_address}"
+            type = "ssh"
+            user = "root"
+            password = "${var.root_user_pw}"
+        }
+
+        inline = [
+            "sudo apt-get update",
+            "curl -fsSL https://get.docker.com -o get-docker.sh",
+            "sudo sh get-docker.sh"
+        ]
     }
+}
