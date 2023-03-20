d803885052d9c6b14a7777bf24bf0857da7d2135
Ibrahim Olanigan <salalite@gmail.com>
2023-03-20 19:30:04 -0400

    Add file provisioner

---
+++ devops/tf/bootstrap-docker.sh
@@ -0,0 +1,4 @@
+sudo apt-get update
+sudo apt-get install git curl -y
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
--- devops/tf/main.tf
+++ devops/tf/main.tf
@@ -22,13 +22,24 @@ resource "linode_instance" "cfe-pyapp" {
     count = "1"
     image = "linode/ubuntu20.04"
     label = "pyapp-${count.index + 1}"
-    group = "Iac-Learner"
+    group = "iac-learner"
     region = "us-east"
     type = "g6-nanode-1"
     authorized_keys = [ var.authorized_key ]
     root_pass = var.root_user_pw
     tags = [ "python", "cfe" ]
 
+    provisioner "file" {
+        connection {
+            host = "${self.ip_address}"
+            type = "ssh"
+            user = "root"
+            password = "${var.root_user_pw}"
+        }
+
+        source = "bootstrap-docker.sh"
+        destination = "/tmp/bootstrap-docker.sh"
+    }
     provisioner "remote-exec" {
         connection {
             host = "${self.ip_address}"
@@ -38,9 +49,8 @@ resource "linode_instance" "cfe-pyapp" {
         }
 
         inline = [
-            "sudo apt-get update",
-            "curl -fsSL https://get.docker.com -o get-docker.sh",
-            "sudo sh get-docker.sh"
+            "chmod +x /tmp/bootstrap-docker.sh",
+            "sudo sh /tmp/bootstrap-docker.sh"
         ]
     }
 }
